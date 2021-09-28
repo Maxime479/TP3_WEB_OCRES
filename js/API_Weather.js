fcd6ac0b40754b276f54caf7d1e8d9bc836df761
Maxime <maxime.saurin@gmail.com>
2021-09-28 21:30:59 +0200

    TP4 Terminé

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -3,15 +3,17 @@
 // Clé api
 const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
 // Url API
-const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_URL = "https://api.openweathermap.org/data/2.5/forecast/daily";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
 
 
 class API_WEATHER{
   constructor(city){
+    city = document.getElementById('city-input').value;
+
     // Si la ville n'est pas définit alors la ville par défault est Paris
-    if(city === undefined){
+    if(city === undefined || city === ""){
       city = "paris";
     }
     this.city = city;
@@ -25,8 +27,17 @@ class API_WEATHER{
       crossdomain: true
     })
   }
+
+  getThreeDayForecast(){
+    return axios
+        .get(`${API_URL}?q=${this.city}&units=metric&cnt=4&appid=${API_KEY}`, {
+          crossdomain: true
+        })
+  }
+
   // Retourne l'element HTML de l'icon symbolisant la méteo.
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
   }
+
 }
--- js/script.js
+++ js/script.js
@@ -6,22 +6,67 @@ function start() {
   // Appel de la fonction fetchTodayForecast
 
   apiWeather
-    .fetchTodayForecast()
+    .getThreeDayForecast()
     .then(function(response) {
       // Récupère la donnée d'une API
       const data = response.data;
+        console.log(data);
 
-      // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+        var main;
+        var descriptionn;
+        var temp;
+        var icon;
 
-      // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+        function next(stringDay){
+
+            var mainString = stringDay + '-forecast-main';
+            var descriptionString = stringDay + '-forecast-more-info';
+            var iconString = 'icon-weather-container-' + stringDay;
+            var tempString = stringDay + '-forecast-temp';
+
+            document.getElementById(mainString).innerHTML = main;
+            document.getElementById(descriptionString).innerHTML = descriptionn;
+            document.getElementById(iconString).innerHTML = icon;
+            document.getElementById(tempString).innerHTML = `${temp}°C`;
+        }
+
+
+        function refresh(intDay){
+
+            main = data.list[intDay].weather[0].main;
+            descriptionn = data.list[intDay].weather[0].description;
+            temp = data.list[intDay].temp.day;
+            icon = apiWeather.getHTMLElementFromIcon(data.list[intDay].weather[0].icon);
+
+            var stringDay;
+
+            switch (intDay){
+                case 0:
+                    stringDay = 'today';
+                    break;
+                case 1:
+                    stringDay = 'tomorrow';
+                    break;
+                case 2:
+                    stringDay = 'after-tomorrow';
+                    break;
+                case 3:
+                    stringDay = 'again-after-tomorrow';
+                    break;
+                default:
+                    console.log("error in switch - function refresh");
+                    break;
+            }
+
+            next(stringDay);
+        }
+
+        for(i=0; i<=4; i++){
+            refresh(i);
+        }
+
+
+
       
     })
     .catch(function(error) {
